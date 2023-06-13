bb156bedd77b790ef5e282f5072ba5b9e1d4b84e
Varush Siva <106482656+VarushSiva@users.noreply.github.com>
2023-06-13 06:03:12 -0400

    Add Chained Equation & Keypress

--- main.js
+++ main.js
@@ -31,79 +31,107 @@ let numbers = document.querySelectorAll(".number")
 let prevScreen = document.querySelector(".prev");
 let currentScreen = document.querySelector(".current")
 
+window.addEventListener('keydown', handleKeyPress)
+
 numbers.forEach(number => number.addEventListener('click', (e) => {
     handleNumber(e.target.textContent);
-    currentScreen.textContent = currentValue;
 }));
 
 function handleNumber(num) {
     if (currentValue.length < 7) {
         currentValue += num;
     }
+    currentScreen.textContent = currentValue;
 }
 
 operator.forEach(op => op.addEventListener("click", (e) => {
     if (currentValue !== "") {
         handleOperator(e.target.textContent);
-        prevScreen.textContent = `${previousValue} ${operation}`;
-        currentScreen.textContent = currentValue;
     }
 }))
 
 function handleOperator(op) {
-    operation = op;
-    previousValue = currentValue;
+    if (previousValue === "") {
+        operation = op;
+        previousValue = currentValue;
+        prevScreen.textContent = `${previousValue} ${operation}`;
+    } else if (currentValue === "") {
+        operation = op;
+        previousValue = "0";
+        prevScreen.textContent = `${previousValue} ${operation}`;
+    } else {
+        calculate();
+        operation = op;
+        prevScreen.textContent = `${previousValue} ${operation}`;
+    }
     currentValue = "";
+    currentScreen.textContent = currentValue;
 }
 
-clear.addEventListener("click", () => {
+clear.addEventListener("click", allClear)
+
+function allClear() {
     previousValue = "";
     operation = "";
     currentValue = "";
     prevScreen.textContent = previousValue;
     currentScreen.textContent = currentValue;
-})
+}
 
-del.addEventListener("click", () => {
+del.addEventListener("click", handleDelete)
+
+function handleDelete() {
     if (currentValue.length > 0) {
         currentValue = currentValue.slice(0,-1)
     }
     currentScreen.textContent = currentValue;
-})
+}
 
 evaluate.addEventListener("click", () => {
+    evaluation();
+})
+
+function evaluation() {
     if (currentValue !== "" && previousValue !== "") {
         calculate();
-        if (currentValue.length < 14) {
-            currentScreen.textContent = currentValue;
-        } else {
-            currentScreen.textContent = currentValue.slice(0,14) + "...";
-        }
+        currentScreen.textContent = previousValue;
+        previousValue = "";
+        prevScreen.textContent = "";
     }
-})
+}
 
 function calculate() {
     previousValue = Number(previousValue);
     currentValue = Number(currentValue);
     if (operation === "+") {
-        currentValue = add(previousValue, currentValue);
+        previousValue = add(previousValue, currentValue);
     } else if (operation === "-") {
-        currentValue = subtract(previousValue, currentValue);
+        previousValue = subtract(previousValue, currentValue);
     } else if (operation === "x") {
-        currentValue = multiply(previousValue, currentValue);
+        previousValue = multiply(previousValue, currentValue);
     } else if (operation === "÷") {
-        currentValue = divide(previousValue, currentValue);
+        if (previousValue === 0 || currentValue === 0) {
+            previousValue = "Error!"
+            return previousValue;
+        }
+        previousValue = divide(previousValue, currentValue);
     } else if (operation === "%") {
-        currentValue = modulo(previousValue, currentValue);
+        previousValue = modulo(previousValue, currentValue);
     }
 
-    currentValue = roundNumber(currentValue).toString();
-    previousValue = "";
-    prevScreen.textContent = "";
-    currentScreen.textContent = currentValue;
+    previousValue = roundNumber(previousValue).toString();
+
+    if (previousValue.length < 14) {
+        return previousValue;
+    } else {
+        previousValue =  previousValue.slice(0,14) + "...";
+        return previousValue;
+    }
 }
 
-decimal.addEventListener("click", () => {
+decimal.addEventListener("click", addDecimal)
+
+function addDecimal() {
     if (currentValue.length === 0) {
         currentValue += "0."
     }
@@ -111,8 +139,34 @@ decimal.addEventListener("click", () => {
         currentValue += ".";
     }
     currentScreen.textContent = currentValue;
-})
+}
 
 function roundNumber(num) {
     return Math.round(num*1000)/1000;
 }
+
+function handleKeyPress(e) {
+    e.preventDefault();
+    if (e.key >= 0 && e.key <=9) {
+        handleNumber(e.key);
+    }
+    if ((e.key  === "Enter" || e.key === "=") && (currentValue !== "" && previousValue !== "")) {
+        evaluation();
+    }
+    if (e.key === "+" || e.key === "-" || e.key === "/") {
+        handleOperator(e.key);
+    }
+    if (e.key === "*") {
+        handleOperator("x");
+    }
+    if (e.key === ".") {
+        addDecimal();
+    }
+    if (e.key === "Backspace") {
+        handleDelete();
+    }
+    if (e.key === "Escape") {
+        allClear();
+    }
+}
+
